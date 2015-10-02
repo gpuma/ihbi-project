e12898687e0f1b5b522f6345bc3dcfd194a5b670
x4722425 <x4722425@hotmail.com>
2015-10-02 13:14:38 +1000

    added exercise view

---
+++ Views/ExerciseView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace ihbiproject
+{
+	public class ExerciseView : ContentPage
+	{
+		public ExerciseView ()
+		{
+			this.Title = "Exercise";
+			Content = new TableView { 
+				Root = new TableRoot{
+					new TableSection{
+						new EntryCell {Label = "Minutes exercised"},
+						new SwitchCell {Text = "Stretching"},
+						new SwitchCell {Text = "Pelvic floor exercise"},
+						new EntryCell {Placeholder = "Type of Exercise"}
+
+					}
+				},
+				Intent = TableIntent.Settings
+			};
+		}
+	}
+}
+
+
