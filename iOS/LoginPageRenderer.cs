205ee0273532ec38e8c9376aa31a31f68731f31f
x4722425 <x4722425@hotmail.com>
2015-10-29 09:16:24 +1000

    iOS little changes

--- iOS/LoginPageRenderer.cs
+++ iOS/LoginPageRenderer.cs
@@ -18,13 +18,15 @@ namespace ihbiproject.iOS
 		{
 			base.ViewDidAppear (animated);
 
+
 			var auth = new OAuth2Authenticator (
 				clientId: "621549137987350", // your OAuth2 client id
 				scope: "", // the scopes for the particular API you're accessing, delimited by "+" symbols
 				authorizeUrl: new Uri ("https://m.facebook.com/dialog/oauth/"), // the auth URL for the service
 				redirectUrl: new Uri ("http://localhost:3000/")); // the redirect URL for the service
 
-			auth.Completed += (sender, eventArgs) => {
+
+			auth.Completed += async(sender, eventArgs) => {
 				DismissViewController (true, null);
 
 				if (eventArgs.IsAuthenticated) {
@@ -32,8 +34,8 @@ namespace ihbiproject.iOS
 					var expiresIn = Convert.ToDouble (eventArgs.Account.Properties ["expires_in"]);
 					var expiryDate = DateTime.Now + TimeSpan.FromSeconds (expiresIn);
 
-					var request = new OAuth2Request ("GET", new Uri ("http://graph.facebook.com/me"), null, eventArgs.Account);
-					var response = await request.GetResponseAsync ());
+					var request = new OAuth2Request ("GET", new Uri ("https://www.facebook.com/groups/youngerwomenwellness/"), null, eventArgs.Account);
+					var response = await request.GetResponseAsync ();
 					var obj = JObject.Parse (response.GetResponseText ());
 
 					var id = obj ["id"].ToString ().Replace ("\"", "");
