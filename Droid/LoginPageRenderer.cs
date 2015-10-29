e02bfc09f13bf9cc66c631469a70f6e6d5e03093
GeoffreyByrne <giririsss@giririsss.com>
2015-10-29 23:17:33 +1000

    FB Login for Androidworking
    Finally got it working.

--- Droid/LoginPageRenderer.cs
+++ Droid/LoginPageRenderer.cs
@@ -15,43 +15,38 @@ namespace ihbiproject.Droid
 {
 	public class LoginPageRenderer : PageRenderer
 	{
-		public LoginPageRenderer()
+		protected override void OnElementChanged (ElementChangedEventArgs<Page> e)
 		{
-
-			string json = @"{""name"":""Hayley Lee"",""id"":""115399638820888""}";
-			System.Diagnostics.Debug.WriteLine ("======>json = "+ json);
-			var jsonObj = JObject.Parse (json);
-
-
-			System.Diagnostics.Debug.WriteLine ("======>in Android LoginPageRenderer");
+			base.OnElementChanged (e);
 			var activity = this.Context as Activity;
-
 			var auth = new OAuth2Authenticator (
 				clientId: "621549137987350", // your OAuth2 client id
 				scope: "", // the scopes for the particular API you're accessing, delimited by "+" symbols
 				authorizeUrl: new Uri ("https://m.facebook.com/dialog/oauth/"), // the auth URL for the service
-				redirectUrl: new Uri ("http://www.facebook.com/connect/login_success.html")); // the redirect URL for the service
-			System.Diagnostics.Debug.WriteLine ("======>in after OAuth2");
+				redirectUrl: new Uri ("http://ihbiproject.azurewebsites.net/api/Users")); // the redirect URL for the service
 			auth.Completed += async (sender, eventArgs) => {
 				if (eventArgs.IsAuthenticated) {
 					var accessToken = eventArgs.Account.Properties ["access_token"].ToString ();
 					var expiresIn = Convert.ToDouble (eventArgs.Account.Properties ["expires_in"]);
 					var expiryDate = DateTime.Now + TimeSpan.FromSeconds (expiresIn);
-
 					var request = new OAuth2Request ("GET", new Uri ("http://graph.facebook.com/me"), null, eventArgs.Account);
 					var response = await request.GetResponseAsync ();
 					var obj = JObject.Parse (response.GetResponseText ());
-
 					var id = obj ["id"].ToString ().Replace ("\"", "");
 					var name = obj ["name"].ToString ().Replace ("\"","");
-
 					App.SuccessfulLoginAction.Invoke();
-
 				} else {
 
 				}
 			};
 			System.Diagnostics.Debug.WriteLine ("======>after OAuth");
+			activity.StartActivity(auth.GetUI(activity));
+	
+		}
+
+
+		public LoginPageRenderer()
+		{
 
 		}
 	}
