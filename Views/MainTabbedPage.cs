c747ea645aa3f8ab786a96009421fbbd46d6c033
x4722425 <x4722425@hotmail.com>
2015-10-30 08:29:55 +1000

    iOS changes

--- Views/MainTabbedPage.cs
+++ Views/MainTabbedPage.cs
@@ -9,6 +9,7 @@ namespace ihbiproject
 	{
 		public MainTabbedPage ()
 		{
+			
 			this.Children.Add (new NewsFeedView () { Title = "News Feed", Icon = "new_icon.png" });
 			this.Children.Add (new DailyCheckinView () { Title = "Daily Checkin", Icon = "dailycheckin.png" });
 			this.Children.Add (new ScheduleView () { Title = "Schedule", Icon = "calendar.png" });
--- iOS/LoginPageRenderer.cs
+++ iOS/LoginPageRenderer.cs
@@ -5,6 +5,7 @@ using Xamarin.Forms;
 using ihbiproject;
 using UIKit;
 using Newtonsoft.Json.Linq;
+using ihbiproject.Views;
 
 
 [assembly: ExportRenderer (typeof (ihbiproject.LoginPage), typeof (ihbiproject.iOS.LoginPageRenderer))]
@@ -23,7 +24,7 @@ namespace ihbiproject.iOS
 				clientId: "621549137987350", // your OAuth2 client id
 				scope: "", // the scopes for the particular API you're accessing, delimited by "+" symbols
 				authorizeUrl: new Uri ("https://m.facebook.com/dialog/oauth/"), // the auth URL for the service
-				redirectUrl: new Uri ("http://localhost:3000/")); // the redirect URL for the service
+				redirectUrl: new Uri ("http://ihbiproject.azurewebsites.net/api/Users")); // the redirect URL for the service
 
 
 			auth.Completed += async(sender, eventArgs) => {
@@ -31,13 +32,16 @@ namespace ihbiproject.iOS
 
 				if (eventArgs.IsAuthenticated) {
 					var accessToken = eventArgs.Account.Properties ["access_token"].ToString ();
+					System.Diagnostics.Debug.WriteLine (accessToken);
+			
 					var expiresIn = Convert.ToDouble (eventArgs.Account.Properties ["expires_in"]);
 					var expiryDate = DateTime.Now + TimeSpan.FromSeconds (expiresIn);
-
-					var request = new OAuth2Request ("GET", new Uri ("https://www.facebook.com/groups/youngerwomenwellness/"), null, eventArgs.Account);
+					var request = new OAuth2Request ("GET", new Uri ("https://graph.facebook.com/me"), null, eventArgs.Account);
 					var response = await request.GetResponseAsync ();
-					var obj = JObject.Parse (response.GetResponseText ());
 
+					System.Diagnostics.Debug.WriteLine ("======>before jobject");
+					var obj = JObject.Parse (response.GetResponseText ());
+					System.Diagnostics.Debug.WriteLine ("======>after jobject");
 					var id = obj ["id"].ToString ().Replace ("\"", "");
 					var name = obj ["name"].ToString ().Replace ("\"","");
 
