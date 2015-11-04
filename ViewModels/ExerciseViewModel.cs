547dd8570cfae8ebed09d6db9b2ad19e94863f51
x4722425 <x4722425@hotmail.com>
2015-11-04 20:55:55 +1000

    nothing, just to sync

--- ViewModels/ExerciseViewModel.cs
+++ ViewModels/ExerciseViewModel.cs
@@ -7,13 +7,13 @@ namespace ihbiproject.ViewModels
 {
 	public class ExerciseViewModel
 	{
-
 			public string exerciseType { get; set;}
 			public int exerciseMin { get; set;}
 			public bool stretching { get; set;}
 			public bool pelvic { get; set;}
-
 	}
+
+
 }
 
 
