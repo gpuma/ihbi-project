b2279fbe1eacd8ee6713b908adbb6ba11bef56e8
x4722425 <x4722425@hotmail.com>
2015-11-05 06:14:33 +1000

    iOS fb link (not working yet)

--- iOS/FBLink_iOS.cs
+++ iOS/FBLink_iOS.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Rivets;
+using ihbiproject;
 
 [assembly: Xamarin.Forms.Dependency(typeof(ihbiproject.iOS.FBLink_iOS))]
 namespace ihbiproject.iOS
@@ -11,7 +13,15 @@ namespace ihbiproject.iOS
         {
             //NSURL* url = [NSURL URLWithString: @"fb://profile/<id>"];
             //[[UIApplication sharedApplication] openURL:url];
-            throw new NotImplementedException();
+			System.Diagnostics.Debug.WriteLine ("======>before applinks");
+			System.Diagnostics.Debug.WriteLine ("======>URL" + URI);
+			string token = App.Instance.Token;
+			System.Diagnostics.Debug.WriteLine ("token  " + token);
+
+			Rivets.AppLinks.DefaultResolver = new FacebookIndexAppLinkResolver ("621549137987350", token);
+			Rivets.AppLinks.Navigator.Navigate(URI);
+			System.Diagnostics.Debug.WriteLine ("======>after applinks");
+
         }
     }
 }
