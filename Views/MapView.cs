55e14e8570d55b5e49dad384edacbb58d73706fd
vash47 (Gustavo) <vash47@gmail.com>
2015-11-05 12:44:09 +1000

    getting current location now

--- Views/MapView.cs
+++ Views/MapView.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Emit;
 using System.Text;
-
+using Geolocator.Plugin;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
 using ihbiproject.Data;
@@ -13,14 +13,17 @@ namespace ihbiproject.Views
     public class MapView : ContentPage
     {
         //gardens point coordinates
-        //todo: change so we get current location
-        Position startPos = new Position(-27.478033, 153.028755);
+        //Position startPos = new Position(-27.478033, 153.028755);
+        Position currentPos;
         Distance startZoom = Distance.FromKilometers(.8);
+        Label txtPosition = new Label();
         public MapView()
         {
-            //var txtPosition = new Entry();
+            var btnLocation = new Button() { Text = "Get location" };
+            UpdateGeoLocation();
+            btnLocation.Clicked += BtnLocation_Clicked;
             var map = new Map(MapSpan.FromCenterAndRadius(
-                startPos, startZoom))
+                currentPos, startZoom))
             {
                 IsShowingUser = true,
                 HeightRequest = 100,
@@ -35,9 +38,24 @@ namespace ihbiproject.Views
                 map.Pins.Add(p);
             }
             var stack = new StackLayout { Spacing = 0 };
-            //stack.Children.Add(txtPosition);
+            stack.Children.Add(txtPosition);
+            stack.Children.Add(btnLocation);
+            var locator = CrossGeolocator.Current;
+
             stack.Children.Add(map);
             Content = stack;
+        }
+
+        private void BtnLocation_Clicked(object sender, EventArgs e)
+        {
+            UpdateGeoLocation();
+        }
+
+        private async void UpdateGeoLocation()
+        {
+            var geoPos = await CrossGeolocator.Current.GetPositionAsync(timeoutMilliseconds: 10000);
+            currentPos = new Position(geoPos.Latitude, geoPos.Longitude);
+            txtPosition.Text = String.Format("{0},{1}", currentPos.Latitude, currentPos.Longitude);
         }
 
         private async void MapPin_Clicked(object sender, EventArgs e)
