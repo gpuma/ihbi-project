255f5b9e8a607602cf451f547a20a26edf3c9253
vash47 (Gustavo) <vash47@gmail.com>
2015-11-05 20:21:03 +1000

    calendar view working

--- Views/ScheduleView.cs
+++ Views/ScheduleView.cs
@@ -1,20 +1,24 @@
 ﻿using System;
 
 using Xamarin.Forms;
+using XLabs.Forms.Controls;
 
 namespace ihbiproject
 {
-	public class ScheduleView : ContentPage
-	{
-		public ScheduleView ()
-		{
-			Content = new StackLayout { 
-				Children = {
-					new Label { Text = "Hello Schedule Page" }
-				}
-			};
-		}
-	}
+    public class ScheduleView : ContentPage
+    {
+        public ScheduleView()
+        {
+            var calendar = new CalendarView
+            {
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.CenterAndExpand
+            };
+            var stack = new StackLayout { Spacing = 0 };
+            stack.Children.Add(calendar);
+            Content = stack;
+        }
+    }
 }
 
 
